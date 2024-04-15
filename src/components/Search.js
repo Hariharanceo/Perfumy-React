d691302749f0980e5daad81e2e0306d4240df4f7
Hariharanceo <Hariharanceo@gmail.com>
2024-04-15 23:23:43 +0530

    updated files

---
+++ src/components/Search.js
@@ -0,0 +1,8 @@
+function Search(){
+    return(
+      <div className="searchbar">
+        <input type="text" placeholder="Search Anything..."></input>
+      </div>
+    )
+  }
+export default Search
